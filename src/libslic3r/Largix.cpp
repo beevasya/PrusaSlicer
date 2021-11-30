33ee583b9a429d536e59c9db75221e4639c47e5f
Evgeni <evgeni@largix.com>
2021-11-30 17:06:36 +0200

    Provide new path and name for ProgramBuilding Improve Largix export

--- src/libslic3r/Largix.cpp
+++ src/libslic3r/Largix.cpp
@@ -19,63 +19,72 @@
 #include <TeddySliceConvert.h>
 
 
-namespace Slic3r {
-
-bool LargixExport::do_export(Print *print, const char *path)
+namespace Slic3r 
 {
-    Largix::Slices          slices;
-    Largix::ConvertSettings settings = Largix::DefaultSettings::getDefaultSettings();
 
+    void LargixExport::fillSettings(const PrintObjectConfig &config, Largix::ConvertSettings &set)
+    {
+        set = Largix::DefaultSettings::getDefaultSettings();
 
-    for (auto object : print->objects()) {
-        std::vector<GCode::LayerToPrint> layers_to_print;
-        layers_to_print.reserve(object->layers().size() +
-                                object->support_layers().size());
-        for (auto layer : object->layers()) {
-            Largix::Slice slice;
-            slice.setZ(layer->slice_z);
-            for (auto region : layer->regions()) {
-                auto pLines = region->fills.as_polylines();
-                std::vector<Largix::Point2D> line_out;
-                for (auto line : pLines) 
-                {
-                    LargixHelper::convertPolylineToLargix(line, line_out);
-                }
-                slice.swapPoints(line_out);
-            }
-            settings.laserRotationAxisOffset =
-                object->config().largix_laser_rotation_axis_offset;
-            settings.laserRotationRadius = object->config()
-                                               .largix_laser_rotation_radius;
-            settings.laserSpotOffset = object->config()
-                                           .largix_laser_spot_offset;
-            settings.wheelAxisOffset = object->config()
-                                           .largix_wheel_axis_offset;
-            settings.wheelRotationRadius = object->config()
-                                            .largix_wheel_rotation_radius;
+        set.laserRotationAxisOffset = config.largix_laser_rotation_axis_offset;
+        set.laserRotationRadius = config.largix_laser_rotation_radius;
+        set.laserSpotOffset = config.largix_laser_spot_offset;
+        set.wheelAxisOffset = config.largix_wheel_axis_offset;
+        set.wheelRotationRadius = config.largix_wheel_rotation_radius;
 
-            settings.StrandHeight = object->config()
-                                            .largix_strands_height;
-            settings.StrandWidth = object->config()
-                                            .largix_strands_width;
-            settings.toolOffset = object->config()
-                                            .largix_tool_offset;
-            if (object->config().largix_angle_pattern)
-            {
-                settings.stairMode = object->config().largix_stair_mode;
-            }
-            Largix::TeddySliceConvert conv(slice, settings);
-            conv.convert();
-            slices.push_back(conv.getSlice());
-        }
+        set.StrandHeight = config.largix_strands_height;
+        set.StrandWidth  = config.largix_strands_width;
+        set.toolOffset   = config.largix_tool_offset;
+        set.toolOffset   = config.largix_stair_mode;
+       
     }
 
-    Largix::TeddyConvert convert(slices, settings);
-    if (!convert.convert()) { return false; }
+    size_t LargixExport::getNumOfSlices(const Print* print) 
+    { 
+        size_t num = 0;
+        for (auto object : print->objects())
+            for (auto layer : object->layers())
+                num += layer->regions().size();
 
-    if (!writeTeddyCSV(path, convert.getProgram())) { return false; }
+        return num;
+    }
 
-    return true;
-}
+    bool LargixExport::do_export(Print *print, const char *path)
+    {
+        size_t num = getNumOfSlices(print);
+        if (num == 0) return false;
+        Largix::Slices slices(num);
+
+        const auto& objects = print->objects();
+        size_t i = 0;
+        for (auto object : objects) 
+        {
+            for (auto layer : object->layers()) 
+            {
+                for (auto region : layer->regions()) 
+                {
+                    slices[i].setZ(layer->slice_z);
+                    auto pLines = region->fills.as_polylines();
+                    std::vector<Largix::Point2D> line_out;
+                    for (auto line : pLines) 
+                    {
+                        LargixHelper::convertPolylineToLargix(line, line_out);
+                    }
+                    slices[i].swapPoints(line_out);
+                    i++;
+                }
+            }
+        }
+
+        Largix::ConvertSettings settings;
+        fillSettings(objects.front()->config(), settings);
+
+        Largix::TeddyConvert convert(slices, settings);
+        if (!convert.convert()) { return false; }
+
+        if (!writeTeddyCSV(path, convert.getProgram())) { return false; }
+
+        return true;
+    }
 
 } // namespace Slic3r

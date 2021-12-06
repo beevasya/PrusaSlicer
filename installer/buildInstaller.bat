2f3765650af2bc2a468378fb99552f6a179634b6
Evgeni <evgeni@largix.com>
2021-12-06 09:59:44 +0200

    Change dll name

--- installer/buildInstaller.bat
+++ installer/buildInstaller.bat
@@ -4,7 +4,7 @@ set PRUSA_PATH=C:\MyProj\PrusaSlicer
 set PRUSA_DEP_PATH=C:\MyProj\PrusaSlicer-deps
 rem set INSTALL_DIR=C:\Temp\inst
 set INSTALL_DIR=G:\Shared drives\Engineering\7. Software\Installs\PrusaLargix
-set PRUSA_VER=1.0.1
+set PRUSA_VER=1.0.2
 
 
 %INSTALL_CC% %PRUSA_PATH%\installer\LargixSlicerScript.iss

ce384fd7c0483bdfe3f57697181df1be4f0d05e0
Evgeni <evgeni@largix.com>
2021-12-18 18:24:22 +0200

    version

--- installer/buildInstaller.bat
+++ installer/buildInstaller.bat
@@ -2,9 +2,8 @@ set INSTALL_CC="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 set LARGIX_PATH=C:\MyProj\TeddyTools
 set PRUSA_PATH=C:\MyProj\PrusaSlicer
 set PRUSA_DEP_PATH=C:\MyProj\PrusaSlicer-deps
-rem set INSTALL_DIR=C:\Temp\inst
 set INSTALL_DIR=G:\Shared drives\Engineering\7. Software\Installs\PrusaLargix
-set PRUSA_VER=1.0.2
+set PRUSA_VER=1.0.9
 
 
 %INSTALL_CC% %PRUSA_PATH%\installer\LargixSlicerScript.iss

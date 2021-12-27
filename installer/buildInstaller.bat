29bb2503587a890a59a40752dc7691f5e3e0164d
Evgeni <evgeni@largix.com>
2021-12-27 10:17:48 +0200

    Support LargixBase defs

--- installer/buildInstaller.bat
+++ installer/buildInstaller.bat
@@ -3,7 +3,7 @@ set LARGIX_PATH=C:\MyProj\TeddyTools
 set PRUSA_PATH=C:\MyProj\PrusaSlicer
 set PRUSA_DEP_PATH=C:\MyProj\PrusaSlicer-deps
 set INSTALL_DIR=G:\Shared drives\Engineering\7. Software\Installs\PrusaLargix
-set PRUSA_VER=1.0.11
+set PRUSA_VER=1.0.12
 
 
 %INSTALL_CC% %PRUSA_PATH%\installer\LargixSlicerScript.iss

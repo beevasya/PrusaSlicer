55199a95486fe9f4429a2f218b75db265e2cf1cc
Oleg Mamrykin <movesoft@yandex.ru>
2022-01-18 23:55:16 +0400

    Update CLITool.bat

--- batch/CLITool.bat
+++ batch/CLITool.bat
@@ -1,8 +1,11 @@
-rem all filenames must not contain spaces
 @echo off
+rem all filenames must not contain spaces
 rem on the next line, you can specify the path to the prusa-slicer-console.exe file, 
 rem if it is not specified in the PATH environment variable
-set PRUSA_CONSOLE_PATH=C:\src\PrusaSlicer\build\src\Release
+set PRUSA_CONSOLE_PATH=%PRUSA_PATH%\build\src\Release
+set PROGRAM_CHECKER_PATH=%LARGIX_PATH%\ProgramBuilder\x64\Release
+echo %PRUSA_CONSOLE_PATH%
+echo %PROGRAM_CHECKER_PATH%
 set CSVEXT=.csv
 set LOGEXT=.log
 if "%~2"=="" (
@@ -10,6 +13,7 @@ if "%~2"=="" (
 	echo Example: clitool models.txt settings.txt "--center 40,40"
 	echo Warning: All filenames must be without spaces
 	echo The path to the prusa-slicer-console.exe file must be specified in the PATH variable
+	echo or in the PRUSA_CONSOLE_PATH variable in this batch file
 	exit /b 0
 )
 for /f "delims=" %%I in (%2) do (
@@ -33,7 +37,11 @@ for /f "delims=" %%I in (%2) do (
 				rem ren %%~nF%CSVEXT% %%~nF_%%~nI%CSVEXT%
 				ren output.csv %%~nF_%%~nI%CSVEXT%
 				echo Checking program...
-				ProgramCheckerTool.exe %%~nF_%%~nI%CSVEXT% %%I >nul
+				if exist %PROGRAM_CHECKER_PATH%\ProgramCheckerTool.exe (
+					%PROGRAM_CHECKER_PATH%\ProgramCheckerTool.exe %%~nF_%%~nI%CSVEXT% %%I >nul
+				) else (
+					ProgramCheckerTool.exe %%~nF_%%~nI%CSVEXT% %%I >nul
+				)
 				if ERRORLEVEL 2 (
 					echo Program %%~nF_%%~nI%CSVEXT% is invalid. See log file %%~nF_%%~nI%LOGEXT% for details.
 				) else (
